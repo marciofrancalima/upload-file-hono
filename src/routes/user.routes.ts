712dc677a32a8c62a36c6059570a6412014d7526
Márcio França Lima <marciofrancalima@hotmail.com>
2024-05-11 18:47:17 -0300

    feat: create controllers/routes for users

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,27 @@
+import { Hono } from "hono";
+
+/**
+ * Routes for users App
+ */
+
+const usersApp = new Hono();
+
+/**
+ * Token validation (/users/*)
+ */
+
+usersApp.get("/validate-token", (c) => {
+  const payload = c.get("jwtPayload");
+  return c.json({ ok: true, userId: payload.id });
+});
+
+/**
+ * Get user by id (/users/:userId)
+ */
+
+usersApp.get("/:userId", (c) => {
+  const userData = c.get("jwtPayload");
+  return c.json(userData);
+});
+
+export default usersApp;
