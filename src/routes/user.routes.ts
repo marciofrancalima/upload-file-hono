77f74fe4fd57516a86339bb2990aec25efbaac5e
Márcio França Lima <marciofrancalima@hotmail.com>
2024-05-18 10:19:03 -0300

    feat: create get all users controller

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { createUser, getUserById, getUsers } from "../services/user.services";
 
 /**
  * Routes for users App
@@ -13,6 +14,15 @@ const usersApp = new Hono().basePath("/users");
 usersApp.get("/validate-token", (c) => {
   const payload = c.get("jwtPayload");
   return c.json({ ok: true, userId: payload.id });
+});
+
+/**
+ * Get all users (/users)
+ */
+
+usersApp.get("/", async (c) => {
+  const users = await getUsers();
+  return c.json(users);
 });
 
 /**
